f2f56eaf7fcec0d65d3972e3cd531e959707c681
Mohammed-Ziyad <mohammedaman8990@gmail.com>
2024-06-12 02:36:04 +0300

    Update build configuration and add publicPath and lintOnSave settings

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+
+module.exports = defineConfig({
+  outputDir: "docs",
   publicPath: "./",
   lintOnSave: false,
-};
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
+      }),
+    ],
+  },
+});
